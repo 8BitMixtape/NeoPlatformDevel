ffae96028e1b8c95f175cf64be5330770402f3f5
Budi Prakosa <iyok@deadmediafm.org>
2018-02-16 23:13:00 +0700

    enable name and desc update

--- mixtapes/mixtapes/components/MixtapeEdit.php
+++ mixtapes/mixtapes/components/MixtapeEdit.php
@@ -25,18 +25,58 @@ class MixtapeEdit extends ComponentBase
     }
     
     public function onRun(){
+
+        $user = Auth::getUser();
+
         $this->mixtape = $this->loadMixtape();
         $this->page['mixtape'] = $this->mixtape;
+        
+        //todo use middleware
+        if($user->id != $this->mixtape->user_id) {
+            return Redirect::back();               
+        }
+
     }
 
     public function onUpdate(){
-        dd("die");
+
+        $zip = new \ZipArchive;
+
+        $user = Auth::getUser();
+        $id = Request::segment(4);
+        $hex_file = Input::file('hex_file');
+
+        $mixtape = Mixtape::find($id);
+
+        if($user->id != $mixtape->user_id) {
+            return Redirect::back();               
+        }
+
+        $validator = Validator::make(
+            $form = Input::all(), [
+               'name' => 'required',
+               'description' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator)->withInput();;   
+        }
+        
+        $hex_file = Input::file('hex_file');
+
+        
+
+        $mixtape->name = Input::get('name');
+        $mixtape->description = Input::get('description');
+        $mixtape->save();
+
     }
  
     protected function loadMixtape(){
-
         $id = Request::segment(4);
         $query = Mixtape::find($id);
+        
         return $query;
     }
 
