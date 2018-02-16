12dcd90d9e7fec12396157a155722caf3f193081
Budi Prakosa <iyok@deadmediafm.org>
2018-02-17 06:25:49 +0700

    add zip file verification

--- mixtapes/mixtapes/components/MixtapeUpload.php
+++ mixtapes/mixtapes/components/MixtapeUpload.php
@@ -29,20 +29,11 @@ class MixtapeUpload extends ComponentBase
         $zip = new \ZipArchive;
         $mixtape = new Mixtape();
 
+        $form = Input::all();
         $hex_file = Input::file('hex_file');
 
 
-        $validator = Validator::make(
-            $form = Input::all(), [
-               'name' => 'required',
-               'description' => 'required',
-               'hex_file' => 'required'
-            ]
-        );
-
-        if ($validator->fails()) {
-            return Redirect::back()->withErrors($validator)->withInput();;   
-        }
+        if($hex_file){
 
         $hex_filename = $hex_file->getClientOriginalName();
         $hex_path = $hex_file->getPathName();
@@ -52,41 +43,64 @@ class MixtapeUpload extends ComponentBase
         $hex_hex_filename = $hex_ino_filename . '.hex';
         $hex_main_filename = $hex_ino_filename . '.cpp';
 
+
+            if ($zip->open($hex_path) === TRUE) {
+
+                $wav_string = $zip->getFromName($hex_wav_filename);
+                $hex_string = $zip->getFromName($hex_hex_filename);
+                $main_string = $zip->getFromName($hex_main_filename);
+
+                $result = preg_match('/(\/\*).*?(\*\/)|(\/\/).*?(\n)/s', $main_string, $matches, PREG_OFFSET_CAPTURE);
+
+                if (($result > 0))
+                {
+                    $info_string = ($matches[0][0]);
+                    $mixtape->info = $info_string;
+                }
+                        
+                if ($wav_string)
+                {
+                    $extracted_wav_file = (new \System\Models\File)->fromData($wav_string, $hex_wav_filename);
+                    $mixtape->wav_file = $extracted_wav_file;
+                }
+
+                if ($hex_string)
+                {
+                    $extracted_hex_file = (new \System\Models\File)->fromData($hex_string, $hex_hex_filename);
+                    $mixtape->hex_file = $extracted_hex_file;
+                }            
+
+                if ($wav_string || $hex_string)
+                {
+                    $form['valid_zip_file'] = 1;
+                }
+
+                $zip->close();
+
+            } else {
+                Flash::error('Zip Extraction Failed!');
+            }
+            
+        }
+        
+
+        $validator = Validator::make(
+            $form, [
+               'name' => 'required',
+               'description' => 'required',
+               'hex_file' => 'required',
+               'valid_zip_file' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator)->withInput();;   
+        }
+
         $mixtape->name = Input::get('name');
         $mixtape->description = Input::get('description');        
         $mixtape->user_id = $user->id;
         $mixtape->zip_file = $hex_file;
-
-        if ($zip->open($hex_path) === TRUE) {
-
-            $wav_string = $zip->getFromName($hex_wav_filename);
-            $hex_string = $zip->getFromName($hex_hex_filename);
-            $main_string = $zip->getFromName($hex_main_filename);
-
-            $result = preg_match('/(\/\*).*?(\*\/)|(\/\/).*?(\n)/s', $main_string, $matches, PREG_OFFSET_CAPTURE);
-
-            if (($result > 0))
-            {
-                $info_string = ($matches[0][0]);
-                $mixtape->info = $info_string;
-            }
-                    
-            if ($wav_string)
-            {
-                $extracted_wav_file = (new \System\Models\File)->fromData($wav_string, $hex_wav_filename);
-                $mixtape->wav_file = $extracted_wav_file;
-            }
-
-            if ($hex_string)
-            {
-                $extracted_hex_file = (new \System\Models\File)->fromData($hex_string, $hex_hex_filename);
-                $mixtape->hex_file = $extracted_hex_file;
-            }            
-
-            $zip->close();
-        } else {
-            Flash::error('Zip Extraction Failed!');
-        }
 
         $mixtape->save();
 
