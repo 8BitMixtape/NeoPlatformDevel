8c8ddedadb5d6634b4e2d65305a42bba21442182
Budi Prakosa <iyok@deadmediafm.org>
2018-02-23 07:47:06 +0700

    initial flarum integration

--- mixtapes/mixtapes/Plugin.php
+++ mixtapes/mixtapes/Plugin.php
@@ -1,18 +1,35 @@
 <?php namespace Mixtapes\Mixtapes;
 
 use System\Classes\PluginBase;
+use Mixtapes\Mixtapes\Classes\Flarum;
+
+use Event;
+use Input;
 
 class Plugin extends PluginBase
 {
+    public function boot()
+    {
+        Event::listen('rainlab.user.login', function($user) {
+            $flarum = new Flarum();
+            $username = $user->username;
+            $email = $user->email;
+            $flarum->login($username, $email);
+        });
+
+        Event::listen('rainlab.user.logout', function($user) {
+            $flarum = new Flarum();
+            $flarum->logout();
+        });
+    }
+
     public function registerComponents()
     {
         return [
             'Mixtapes\Mixtapes\Components\MixtapeUpload' => 'mixtapeupload',
             'Mixtapes\Mixtapes\Components\MixtapeHex' => 'mixtapehex',
             'Mixtapes\Mixtapes\Components\MyMixtape' => 'mymixtape',            
-            'Mixtapes\Mixtapes\Components\MixtapeEdit' => 'mixtapeedit'            
-
-            
+            'Mixtapes\Mixtapes\Components\MixtapeEdit' => 'mixtapeedit'                        
         ];
     }
 
---
+++ mixtapes/mixtapes/classes/config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    // URL to your Flarum forum
+    'flarum_url' => 'http://localhost:8000/neo-platform-devel/community/',
+    // Domain of your main site (without http://)
+    'root_domain' => 'localhost',
+    // Create a random key in the api_keys table of your Flarum forum
+    'flarum_api_key' => 'NotSecureToken',
+    // Random token to create passwords
+    'password_token' => 'NotSecureToken',
+    // How many days should the login be valid
+    'lifetime_in_days' => 14,
+];
+
+?>
