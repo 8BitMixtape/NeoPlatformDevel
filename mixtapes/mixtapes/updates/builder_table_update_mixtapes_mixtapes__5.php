e4bb479c980a3a348e7c6c57275038924ccac024
Budi Prakosa <iyok@deadmediafm.org>
2018-02-25 13:49:17 +0700

    add mixtape flarum_id migration

---
+++ mixtapes/mixtapes/updates/builder_table_update_mixtapes_mixtapes__5.php
@@ -0,0 +1,23 @@
+<?php namespace Mixtapes\Mixtapes\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateMixtapesMixtapes5 extends Migration
+{
+    public function up()
+    {
+        Schema::table('mixtapes_mixtapes_', function($table)
+        {
+            $table->integer('flarum_id')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('mixtapes_mixtapes_', function($table)
+        {
+            $table->dropColumn('flarum_id');
+        });
+    }
+}
