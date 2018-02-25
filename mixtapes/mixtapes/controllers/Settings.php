d694b02a03d7e955a747297faacde67b22f379b3
Budi Prakosa <iyok@deadmediafm.org>
2018-02-25 17:00:15 +0700

    disable debug

--- mixtapes/mixtapes/controllers/Settings.php
+++ mixtapes/mixtapes/controllers/Settings.php
@@ -28,8 +28,8 @@ class Settings extends Controller
            $mixtape_url = url('/mixtape/detail',$mixtape->id);
            $flarum_msg = "Synth name: " . $mixtape->name . "\nUrl: " . $mixtape_url . "\nDescription: \n\n\n\n" . ($mixtape->description);
            $flarum_res_arr = $flarum->createDiscussion($mixtape->user->username, '[mixtape] ' . $mixtape->name, $flarum_msg);
-            print_r($flarum_res_arr);
-            die();
+            // print_r($flarum_res_arr);
+            // die();
            $flarum_id =  $flarum_res_arr['discuss_id'];
            $flarum_post_id =  $flarum_res_arr['post_id'];
            
